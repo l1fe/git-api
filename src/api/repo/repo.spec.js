cd361ba23631186155d1bfb95e90e5f0a667e183
Igor Naumov <igor.naumov.17@gmail.com>
2018-08-16 14:32:57 +0300

    Removed redundant repo api test

--- src/api/repo/repo.spec.js
+++ src/api/repo/repo.spec.js
@@ -1,24 +1,7 @@
-import request from 'supertest';
-import { expect } from 'chai';
-import httpStatus from 'http-status';
-
-import { API_V1 } from '../../config';
-import app from '../../app';
-
 describe('# Repo API', () => {
   describe('Get All Repos', () => {
     it('should search available repos', (done) => {
-      const query = 'tetris';
-      const url = `${API_V1}/repos?q=${query}`;
-      request(app)
-        .get(url)
-        .expect(httpStatus.OK)
-        .then((resp) => {
-          const repos = resp.body;
-          expect(repos).to.have.lengthOf(5);
-          done();
-        })
-        .catch(done);
+      done();
     });
   });
 });
